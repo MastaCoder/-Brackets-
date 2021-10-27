413afa2b03036e43456bd43d4ce23833b79cf2d3
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2021-10-27 14:17:55 -0400

    Refactor of homepage

--- frontend/src/views/Home/HomePage.js
+++ frontend/src/views/Home/HomePage.js
@@ -6,55 +6,53 @@ import styles from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
-    <div className={styles.main}>
+    <Container disableGutters className={styles.main} maxWidth={false}>
       <img src={Background} alt="" className={styles.bg} />
 
-      <Container disableGutters>
-        {/* {Main title} */}
-        <Box
-          sx={{
-            mt: "4rem",
-            mb: "8rem",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h1" className={styles.title}>
-            [Brackets]
-          </Typography>
+      {/* {Main title} */}
+      <Box
+        sx={{
+          mt: "4rem",
+          mb: "8rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h1" className={styles.title}>
+          [Brackets]
+        </Typography>
 
-          <Typography className={styles.titleSub}>
-            Tournament management made easy
-          </Typography>
-        </Box>
+        <Typography className={styles.titleSub}>
+          Tournament management made easy
+        </Typography>
+      </Box>
 
-        {/* {Try it out} */}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h5" className={styles.tryItOut}>
-            What are you waiting for?
-          </Typography>
+      {/* {Try it out} */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5" className={styles.tryItOut}>
+          What are you waiting for?
+        </Typography>
 
-          <Link to="/register" style={{ textDecoration: "none" }}>
-            <Button className={styles.btn} variant="contained" size="large">
-              Register for free
-            </Button>
+        <Link to="/register" style={{ textDecoration: "none" }}>
+          <Button className={styles.btn} variant="contained" size="large">
+            Register for free
+          </Button>
+        </Link>
+
+        <Typography variant="caption" className={styles.regText}>
+          Looking to join a tournament? Register as a player{" "}
+          <Link className={styles.here} to="/register">
+            here!
           </Link>
-
-          <Typography variant="caption" className={styles.regText}>
-            Looking to join a tournament? Register as a player{" "}
-            <Link className={styles.here} to="/register">
-              here!
-            </Link>
-          </Typography>
-        </Box>
-      </Container>
-    </div>
+        </Typography>
+      </Box>
+    </Container>
   );
 }
