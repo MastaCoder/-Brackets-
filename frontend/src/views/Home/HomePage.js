28bd57a61ba574ebe8603ca7691f2bc6f0c01ade
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2021-10-27 21:44:59 -0400

    Refactor with MUI

--- frontend/src/views/Home/HomePage.js
+++ frontend/src/views/Home/HomePage.js
@@ -1,40 +1,31 @@
-import { Button, Container, Typography } from "@mui/material";
-import { Box, fontSize } from "@mui/system";
 import { Link } from "react-router-dom";
 import Background from "../../images/homepageBackground.svg";
 import styles from "./HomePage.module.css";
+import Button from "../../components/Button/Button";
 
 export default function HomePage() {
   return (
-    <div className={styles.main}>
+    <div className={`${styles.main} ${styles.verticalContainer}`}>
       <img src={Background} alt="" className={styles.bg} />
 
       {/* {Main title} */}
-      <Box
-        sx={{
-          mt: "4rem",
-          mb: "8rem",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
+      <div
+        className={`${styles.titleContainerMargins} ${styles.verticalContainer}`}
+        style={{alignItems: "center"}}
       >
         <h1 className={styles.title}>[Brackets]</h1>
         <h5 className={styles.titleSub}>Tournament management made easy</h5>
-      </Box>
+      </div>
 
       {/* {Try it out} */}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
+      <div
+        className={styles.verticalContainer}
+        style={{alignItems: "center"}}
       >
         <h5 className={styles.tryItOut}>What are you waiting for?</h5>
 
         <Link to="/register" style={{ textDecoration: "none" }}>
-          <Button className={styles.btn} variant="contained" size="large">
+          <Button className={styles.btn} cs={{width: "16rem", marginBottom: "1rem"}}>
             Register for free
           </Button>
         </Link>
@@ -45,7 +36,7 @@ export default function HomePage() {
             here!
           </Link>
         </p>
-      </Box>
+      </div>
     </div>
   );
 }
