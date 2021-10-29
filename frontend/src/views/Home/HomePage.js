d2974238de505e1ccc811e634272461c12748e00
Makan <makan@makan.bz>
2021-10-29 12:57:22 -0400

    fixed some stuff

--- frontend/src/views/Home/HomePage.js
+++ frontend/src/views/Home/HomePage.js
@@ -1,32 +1,32 @@
 import { Link } from "react-router-dom";
 import Background from "../../images/homepageBackground.svg";
-import Styles from "./HomePage.module.css";
+import styles from "./HomePage.module.css";
 import Button from "../../components/Button/Button";
 
 export default function HomePage() {
   return (
-    <div className={`${Styles.main} ${Styles.verticalContainer}`}>
-      <img src={Background} alt="" className={Styles.bg} />
+    <div className={`${styles.main} ${styles.verticalContainer}`}>
+      <img src={Background} alt="" className={styles.bg} />
 
       {/* {Main title} */}
       <div
-        className={`${Styles.titleContainerMargins} ${Styles.verticalContainer}`}
+        className={`${styles.titleContainerMargins} ${styles.verticalContainer}`}
         style={{ alignItems: "center" }}
       >
-        <h1 className={Styles.title}>[Brackets]</h1>
-        <h5 className={Styles.titleSub}>Tournament management made easy</h5>
+        <h1 className={styles.title}>[Brackets]</h1>
+        <h5 className={styles.titleSub}>Tournament management made easy</h5>
       </div>
 
       {/* {Try it out} */}
       <div
-        className={Styles.verticalContainer}
+        className={styles.verticalContainer}
         style={{ alignItems: "center" }}
       >
-        <h5 className={Styles.tryItOut}>What are you waiting for?</h5>
+        <h5 className={styles.tryItOut}>What are you waiting for?</h5>
 
         <Link to="/register" style={{ textDecoration: "none" }}>
           <Button
-            className={Styles.btn}
+            className={styles.btn}
             cs={{
               width: "16rem",
               marginBottom: "1rem",
@@ -38,9 +38,9 @@ export default function HomePage() {
           </Button>
         </Link>
 
-        <p className={Styles.regText}>
+        <p className={styles.regText}>
           Looking to join a tournament? Register as a player{" "}
-          <Link className={Styles.here} to="/register">
+          <Link className={styles.here} to="/register">
             here!
           </Link>
         </p>
