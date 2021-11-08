e2df5b42929f978cc62ad83ff9f4cc58d32b8807
aaryapatel <aarya.patel06@gmail.com>
2021-11-07 19:16:00 -0500

    Adding typography to MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,96 +1,124 @@
-import ProvideAuth from "./components/Authentication/ProvideAuth";
-import PrivateRoute from "./components/Authentication/PrivateRoute";
-import Navbar from "./components/Navbar/Navbar";
-import ProfilePage from "./views/Profile/ProfilePage";
-import HomePage from "./views/Home/HomePage";
-import LoginPage from "./views/Login/LoginPage";
-import RegisterPage from "./views/Register/RegisterPage";
-import { createTheme } from "@mui/material/styles";
-import { ThemeProvider } from "@emotion/react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import AdminDashboard from "./views/Dashboards/AdminDashboard/AdminDashboard";
-import UserDashboardPage from "./views/User/Dashboard/UserDashboardPage";
-import UserHistoryPage from "./views/User/History/UserHistoryPage";
-import UserJoinPage from "./views/User/Join/UserJoinPage";
-import OrganizerCreatePage from "./views/Organizer/Create/OrganizerCreatePage";
-import OrganizerHistoryPage from "./views/Organizer/History/OrganizerHistoryPage";
-import AdminViewUserPage from "./views/Admin/Users/AdminViewUsers";
-import AdminViewUserLogs from "./views/Admin/Archive/AdminViewUserLogs";
-import AdminViewTournaments from "./views/Admin/Tournaments/AdminViewTournaments";
-import DataContext from "./contexts/dataContext";
-import data from "./data/data";
-import { useState } from "react";
-import TournamentViewPage from "./views/Tournament/View/TournamentViewPage";
+import ProvideAuth from './components/Authentication/ProvideAuth';
+import PrivateRoute from './components/Authentication/PrivateRoute';
+import Navbar from './components/Navbar/Navbar';
+import ProfilePage from './views/Profile/ProfilePage';
+import HomePage from './views/Home/HomePage';
+import LoginPage from './views/Login/LoginPage';
+import RegisterPage from './views/Register/RegisterPage';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AdminDashboard from './views/Dashboards/AdminDashboard/AdminDashboard';
+import UserDashboardPage from './views/User/Dashboard/UserDashboardPage';
+import UserHistoryPage from './views/User/History/UserHistoryPage';
+import UserJoinPage from './views/User/Join/UserJoinPage';
+import OrganizerCreatePage from './views/Organizer/Create/OrganizerCreatePage';
+import OrganizerHistoryPage from './views/Organizer/History/OrganizerHistoryPage';
+import AdminViewUserPage from './views/Admin/Users/AdminViewUsers';
+import AdminViewUserLogs from './views/Admin/Archive/AdminViewUserLogs';
+import AdminViewTournaments from './views/Admin/Tournaments/AdminViewTournaments';
+import DataContext from './contexts/dataContext';
+import data from './data/data';
+import { useState } from 'react';
+import TournamentViewPage from './views/Tournament/View/TournamentViewPage';
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#6984E1",
-      },
-    },
-  });
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: '#6984E1',
+			},
+		},
+		typography: {
+			h1: {
+				fontFamily: 'Kanit, sans-serif',
+			},
+			h2: {
+				fontFamily: 'Kanit, sans-serif',
+			},
+			h3: {
+				fontFamily: 'Kanit, sans-serif',
+			},
+			h4: {
+				fontFamily: 'Kanit, sans-serif',
+			},
+			h5: {
+				fontFamily: 'Kanit, sans-serif',
+			},
+			h6: {
+				fontFamily: 'Kanit, sans-serif',
+			},
+		},
+	});
 
-  const dataState = useState(data);
+	const dataState = useState(data);
 
-  return (
-    <DataContext.Provider value={dataState}>
-      <ProvideAuth>
-        <ThemeProvider theme={theme}>
-          <Router>
-            <Navbar />
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route exact path="/login">
-                <LoginPage />
-              </Route>
-              <Route exact path="/register">
-                <RegisterPage />
-              </Route>
-              <PrivateRoute exact path="/settings">
-                <ProfilePage
-                  username="user"
-                  email="user@user.com"
-                  password="user"
-                />
-              </PrivateRoute>
-              <PrivateRoute exact path="/dashboard">
-                <AdminDashboard />
-              </PrivateRoute>
-              <PrivateRoute exact path="/admin/users">
-                <AdminViewUserPage />
-              </PrivateRoute>
-              <PrivateRoute exact path="/admin/userLogs">
-                <AdminViewUserLogs />
-              </PrivateRoute>
-              <PrivateRoute exact path="/admin/tournaments/:filter" component={AdminViewTournaments} />
-              <PrivateRoute exact path="/user">
-                <UserDashboardPage />
-              </PrivateRoute>
-              <PrivateRoute exact path="/user/history">
-                <UserHistoryPage />
-              </PrivateRoute>
-              <PrivateRoute exact path="/user/join">
-                <UserJoinPage />
-              </PrivateRoute>
-              <Route exact path="/org/create">
-                <OrganizerCreatePage />
-              </Route>
-              <Route exact path="/org/history">
-                <OrganizerHistoryPage />
-              </Route>
-              <Route exact path="/tournament/:id" component={TournamentViewPage} />
-              {/*<PrivateRoute exact path="/user">*/}
-              {/*  <Dashboard />*/}
-              {/*</PrivateRoute>*/}
-            </Switch>
-          </Router>
-        </ThemeProvider>
-      </ProvideAuth>
-    </DataContext.Provider>
-  );
+	return (
+		<DataContext.Provider value={dataState}>
+			<ProvideAuth>
+				<ThemeProvider theme={theme}>
+					<Router>
+						<Navbar />
+						<Switch>
+							<Route exact path="/">
+								<HomePage />
+							</Route>
+							<Route exact path="/login">
+								<LoginPage />
+							</Route>
+							<Route exact path="/register">
+								<RegisterPage />
+							</Route>
+							<PrivateRoute exact path="/settings">
+								<ProfilePage
+									username="user"
+									email="user@user.com"
+									password="user"
+								/>
+							</PrivateRoute>
+							<PrivateRoute exact path="/dashboard">
+								<AdminDashboard />
+							</PrivateRoute>
+							<PrivateRoute exact path="/admin/users">
+								<AdminViewUserPage />
+							</PrivateRoute>
+							<PrivateRoute exact path="/admin/userLogs">
+								<AdminViewUserLogs />
+							</PrivateRoute>
+							<PrivateRoute
+								exact
+								path="/admin/tournaments/:filter"
+								component={AdminViewTournaments}
+							/>
+							<PrivateRoute exact path="/user">
+								<UserDashboardPage />
+							</PrivateRoute>
+							<PrivateRoute exact path="/user/history">
+								<UserHistoryPage />
+							</PrivateRoute>
+							<PrivateRoute exact path="/user/join">
+								<UserJoinPage />
+							</PrivateRoute>
+							<Route exact path="/org/create">
+								<OrganizerCreatePage />
+							</Route>
+							<Route exact path="/org/history">
+								<OrganizerHistoryPage />
+							</Route>
+							<Route
+								exact
+								path="/tournament/:id"
+								component={TournamentViewPage}
+							/>
+							{/*<PrivateRoute exact path="/user">*/}
+							{/*  <Dashboard />*/}
+							{/*</PrivateRoute>*/}
+						</Switch>
+					</Router>
+				</ThemeProvider>
+			</ProvideAuth>
+		</DataContext.Provider>
+	);
 }
 
 export default App;
