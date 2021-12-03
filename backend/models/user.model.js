26fd63876f6ffaf7bf78b7456e8b3093cba266ff
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2021-12-02 19:57:04 -0500

    Fix for email validation

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,10 +1,5 @@
 import mongoose from "../db/mongoose.js";
-import { whiteSpaceValidation } from "../util/modelValidators.js";
-
-/*
-Regex for email validation gathered from 
-https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
-*/
+import validator from "validator"
 
 
 
@@ -19,14 +14,7 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    validate: {
-      validator: function (v) {
-        return "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])).){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])".test(
-          v
-        );
-      },
-      message: (props) => `${props.v} is not a valid email!`,
-    },
+    validate: [validator.isEmail, "Email is not a valid set of characters."]
   },
   password: {
     type: String,
