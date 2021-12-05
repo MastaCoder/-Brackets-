f94b50c562dc94052e3d2f7d890ec2151e86b97b
Makan <makan@makan.bz>
2021-12-05 15:56:33 -0500

    Update AdminDashboard.js

--- frontend/src/views/Admin/Dashboard/AdminDashboard.js
+++ frontend/src/views/Admin/Dashboard/AdminDashboard.js
@@ -7,114 +7,114 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function AdminDashboard() {
-	const history = useHistory();
+  const history = useHistory();
 
-	const [numActiveUsers, setNumActiveUsers] = useState(0);
-	const [numBannedUsers, setNumBannedUsers] = useState(0);
-	const [numTournaments, setNumTournaments] = useState(0);
-	const [numOngoingTournaments, setNumOngoingTournaments] = useState(0);
+  const [numActiveUsers, setNumActiveUsers] = useState("...");
+  const [numBannedUsers, setNumBannedUsers] = useState("...");
+  const [numTournaments, setNumTournaments] = useState("...");
+  const [numOngoingTournaments, setNumOngoingTournaments] = useState("...");
 
-	useEffect(() => {
-		// Get num active and banned users
-		(async function () {
-			const res = await axios.get('/api/admin/platformusers', {});
-			let active = 0;
-			let banned = 0;
-			res.data.forEach((user) => {
-				user.platformAccess ? (active += 1) : (banned += 1);
-			});
-			setNumActiveUsers(active);
-			setNumBannedUsers(banned);
-		})();
-		// Get num active and all tournaments
-		(async function () {
-			const res = await axios.get('/api/admin/numtournaments', {});
-			setNumTournaments(res.data.open + res.data.ongoing + res.data.closed);
-			setNumOngoingTournaments(res.data.open);
-		})();
-	}, []);
+  useEffect(() => {
+    // Get num active and banned users
+    (async function () {
+      const res = await axios.get('/api/admin/platformusers', {});
+      let active = 0;
+      let banned = 0;
+      res.data.forEach((user) => {
+        user.platformAccess ? (active += 1) : (banned += 1);
+      });
+      setNumActiveUsers(active.toString());
+      setNumBannedUsers(banned.toString());
+    })();
+    // Get num active and all tournaments
+    (async function () {
+      const res = await axios.get('/api/admin/numtournaments', {});
+      setNumTournaments(res.data.open + res.data.ongoing + res.data.closed);
+      setNumOngoingTournaments(res.data.open);
+    })();
+  }, []);
 
-	return (
-		<Container maxWidth="md">
-			<PageTitle>Admin Dashboard</PageTitle>
+  return (
+    <Container maxWidth="md">
+      <PageTitle>Admin Dashboard</PageTitle>
 
-			<PageSubTitle>General Stats</PageSubTitle>
-			<Grid container spacing={2} justifyContent="center">
-				<AdminCounter title="Registered users" number={numActiveUsers} />
-				<AdminCounter title="Banned users" number={numBannedUsers} />
-				<AdminCounter title="# Tournaments" number={numTournaments} />
-				<AdminCounter title="# Ongoing" number={numOngoingTournaments} />
-			</Grid>
-			<Box mt={0.75} mb={3}>
-				<Typography variant="caption" color="grey">
-					Info last updated {Date().toLocaleString()}
-				</Typography>
-			</Box>
+      <PageSubTitle>General Stats</PageSubTitle>
+      <Grid container spacing={2} justifyContent="center">
+        <AdminCounter title="Registered users" number={numActiveUsers} />
+        <AdminCounter title="Banned users" number={numBannedUsers} />
+        <AdminCounter title="# Tournaments" number={numTournaments} />
+        <AdminCounter title="# Ongoing" number={numOngoingTournaments} />
+      </Grid>
+      <Box mt={0.75} mb={3}>
+        <Typography variant="caption" color="grey">
+          Info last updated {Date().toLocaleString()}
+        </Typography>
+      </Box>
 
-			<PageSubTitle>User options</PageSubTitle>
-			<Box mb={4} display="flex" gap={1}>
-				<Button
-					variant="contained"
-					onClick={() => {
-						history.push('/admin/users');
-					}}
-				>
-					Manage Users
-				</Button>
-			</Box>
+      <PageSubTitle>User options</PageSubTitle>
+      <Box mb={4} display="flex" gap={1}>
+        <Button
+          variant="contained"
+          onClick={() => {
+            history.push('/admin/users');
+          }}
+        >
+          Manage Users
+        </Button>
+      </Box>
 
-			<PageSubTitle>Event options</PageSubTitle>
-			<Box mb={4} display="flex" gap={1}>
-				<Button
-					variant="contained"
-					color="info"
-					onClick={() => {
-						history.push('/admin/tournaments/all');
-					}}
-				>
-					View all
-				</Button>
-				<Button
-					variant="contained"
-					color="secondary"
-					onClick={() => {
-						history.push('/admin/tournaments/notstarted');
-					}}
-				>
-					View open events
-				</Button>
-				<Button
-					variant="contained"
-					color="warning"
-					onClick={() => {
-						history.push('/admin/tournaments/ongoing');
-					}}
-				>
-					View On-Going Events
-				</Button>
-			</Box>
+      <PageSubTitle>Event options</PageSubTitle>
+      <Box mb={4} display="flex" gap={1}>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() => {
+            history.push('/admin/tournaments/all');
+          }}
+        >
+          View all
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            history.push('/admin/tournaments/notstarted');
+          }}
+        >
+          View open events
+        </Button>
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={() => {
+            history.push('/admin/tournaments/ongoing');
+          }}
+        >
+          View On-Going Events
+        </Button>
+      </Box>
 
-			<PageSubTitle>Archive</PageSubTitle>
-			<Box mb={4} display="flex" gap={1}>
-				<Button
-					variant="contained"
-					color="info"
-					onClick={() => {
-						history.push('/admin/tournaments/finished');
-					}}
-				>
-					View past tournaments
-				</Button>
-				<Button
-					variant="contained"
-					color="secondary"
-					onClick={() => {
-						history.push('/admin/userLogs');
-					}}
-				>
-					View user logs
-				</Button>
-			</Box>
-		</Container>
-	);
+      <PageSubTitle>Archive</PageSubTitle>
+      <Box mb={4} display="flex" gap={1}>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() => {
+            history.push('/admin/tournaments/finished');
+          }}
+        >
+          View past tournaments
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            history.push('/admin/userLogs');
+          }}
+        >
+          View user logs
+        </Button>
+      </Box>
+    </Container>
+  );
 }
